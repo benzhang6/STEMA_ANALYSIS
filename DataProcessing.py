38777d0e8dc16f6d3bbe11bd1d2e3f1166785aa7
benzhang6 <benjaminzhangwei@gmail.com>
2020-01-16 17:31:32 -0500

    first commit

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,7 +1,19 @@
 import pandas as pd
+import xlrd
 
-df = pd.read_excel("成绩单.xlsx")
-print(df)
+file = "191215-总分.xlsx"
+rb = xlrd.open_workbook(file)
+sheet = rb.sheet_by_index(0) #表示Excel的第一个Sheet
+nrows = sheet.nrows
+print(nrows)
+
+#overall = pd.read_excel("191215-总分.xlsx", sheet_name="总分表", index_col="准考证号")
+#print(overall)
+# detail = pd.read_excel("191215-总分.xlsx", sheet_name="原始成绩录入核算", index_col="准考证号")
+
+#print("The result is :" + overall.iloc[0].size)
+
+"""
 print(df.shape)
 print(df.dtypes)
 print(df.columns)
@@ -11,6 +23,7 @@ print(df["成绩"].unique())
 print(df.isnull())
 # test comment
 print(df.sort_values(by=["成绩"], ascending=False))
+"""
 
 
 
