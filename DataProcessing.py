e9b1ba0442ddf74405208b4747af31429b8f79d2
benzhang6 <benjaminzhangwei@gmail.com>
2020-01-18 11:18:15 -0500

    first commit

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import numpy as np
 
 fileZongFen = "191215-总分.xlsx"
 fileOutput = "tmp_output.xlsx"
+
 
 print("读取 dfZongFen...")
 dfZongFen = pd.read_excel(fileZongFen, sheet_name=0, index_col="准考证号", usecols="A,B")
@@ -11,6 +13,44 @@ print("合并 dfFinal...")
 dfFinal = pd.merge(dfZongFen, dfYuanShi, how="left", on="准考证号")
 
 print("合并之后表格长度：", len(dfFinal))
+
+#判卷
+dfChujiDaan = pd.read_excel(fileZongFen, sheet_name="初级组正确答案")
+dfZhongjiDaan = pd.read_excel(fileZongFen, sheet_name="中高级组正确答案")
+
+npNewScore = np.zeros(dfFinal.shape[0])
+npIsCorrect = np.zeros(dfFinal.shape[0])
+
+for j in range(1059, 1063):
+    nZongfen = 0
+    for i in range(9, 73):
+        if dfFinal.iloc[j]['级别'] == '初级':
+            # print("DEBUG", "初级", nZongfen)
+            if dfFinal.iloc[j]['答案'+str(i)] == dfChujiDaan.iloc[0]['答案'+str(i)] :
+                nZongfen += 2
+            elif dfFinal.iloc[j]['答案' + str(i)] == None or dfFinal.iloc[j]['答案' + str(i)] == "" :
+                pass
+            else:
+                nZongfen -= 1
+        else :
+            #print("DEBUG", "中高级", nZongfen)
+            if dfFinal.iloc[j]['答案' + str(i)] == dfZhongjiDaan.iloc[0]['答案' + str(i)]:
+                nZongfen += 2
+            elif dfFinal.iloc[j]['答案'+str(i)] == None or dfFinal.iloc[j]['答案'+str(i)] == "" :
+               pass
+            else :
+               nZongfen -= 1
+        print("DEBUG", dfFinal.iloc[j]['答案'+str(i)],dfZhongjiDaan.iloc[0]['答案'+str(i)],dfFinal.iloc[j]['成绩'+str(i)])
+
+    if nZongfen != int(dfFinal.iloc[j]['选择总成绩']):
+        npIsCorrect[j] = False
+    else :
+        npIsCorrect[j] = True
+    npNewScore[j] = nZongfen
+    print("正在处理", j, "正确" if npIsCorrect[j] else "错误", "原成绩：", dfFinal.iloc[j]['选择总成绩'], "现成绩：", nZongfen)
+
+dfFinal.insert(0, "新计算选择总成绩", npNewScore)
+dfFinal.insert(0, "正确与否", npIsCorrect)
 
 print("writing excel file...")
 dfFinal.to_excel(fileOutput, sheet_name='Result')
